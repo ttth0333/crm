896e4507adfc48cfa4161914bb9f8280773d4242
Tri Nguyen <ttth0333@gmail.com>
2023-09-11 18:06:15 +0700

    member v2

--- src/main/java/com/cybersoft/crm/api/MemberApi.java
+++ src/main/java/com/cybersoft/crm/api/MemberApi.java
@@ -1,8 +1,40 @@
 package com.cybersoft.crm.api;
 
+import com.cybersoft.crm.payload.ResponseData;
+import com.cybersoft.crm.service.MemberService;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "memberApi", urlPatterns = {"/api/member"})
 public class MemberApi extends HttpServlet {
+    private MemberService memberService = new MemberService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        int id = Integer.parseInt(req.getParameter("id"));
+        boolean isSuccess = memberService.deleteMemberById(id);
+
+        ResponseData responseData = new ResponseData();
+        responseData.setStatus(200);
+        responseData.setSuccess(isSuccess);
+        responseData.setDescription(isSuccess ? "Xoá thành công" : "Xoá thất bại");
+
+        Gson gson = new Gson();
+        //toJson : biến đối tượng thành chuỗi JSON
+        String json = gson.toJson(responseData);
+
+        out.print(json);
+        out.flush();
+    }
 }
--- src/main/java/com/cybersoft/crm/controller/MemberPage.java
+++ src/main/java/com/cybersoft/crm/controller/MemberPage.java
@@ -1,8 +1,21 @@
 package com.cybersoft.crm.controller;
 
+import com.cybersoft.crm.service.MemberService;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "memberPage", urlPatterns = {"/member"})
 public class MemberPage extends HttpServlet {
+    private MemberService memberService = new MemberService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("members", memberService.getAllMembers());
+        req.getRequestDispatcher("/user-table.jsp").forward(req, resp);
+    }
 }
